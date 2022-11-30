3f28db81231107b54805fb25907248bd7ca434ef
Etinosa22 <eekhator25@gmail.com>
2022-11-30 10:22:36 +0100

    A voice recorder

---
+++ recorder/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import Record from "./Record" 
+
+function App(){
+  return(
+    <div> 
+      <Record/>
+    </div>
+  )
+}
+
+export default App
---
+++ recorder/src/Record.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import "./App.css"
+// import React from 'react'
+import vmsg from 'vmsg'
+
+
+//INITIALIZE NEW RECORDER 
+const recorder = new vmsg.Recorder({
+    //WEBASSEMBLYURL
+    wasmURL: 'https://unpkg.com/vmsg@0.4.0/vmsg.wasm'
+  })
+
+class Record extends Component{
+    state = {
+        isLoading:false,
+        isRecording:false,
+        recordings:[]
+      }
+    
+      record = async () => {
+        this.setState({isLoading:true})
+    
+        //Permission tom use Microphone
+        if(this.state.isRecording){
+          const blob = await recorder.stopRecording()
+          this.setState({
+            isLoading:false,
+            isRecording:false,
+            recordings:this.state.recordings.concat(URL.createObjectURL(blob))
+          })
+        }
+        else{
+          try{
+            await recorder.initAudio()
+            await recorder.initWorker()
+            recorder.startRecording()
+            this.setState({isLoading:false,isRecording:true})
+          }catch(e){
+            console.error(e)
+            this.setState({isLoading:false})
+          }
+        }
+      }
+    
+      render(){
+    
+        const {isLoading, isRecording, recordings} = this.state
+    
+        return(
+          <React.Fragment>
+            <div className="cont">
+                <button onClick={this.record} disabled={isLoading} className="btn">{isRecording ? "stop" : "Record"}</button>
+                <ul>
+                    {recordings.map(url => (
+                    <li key={url}>
+                        <audio src={url} controls></audio>
+                    </li>
+                    ))}
+                </ul>
+           </div>
+          </React.Fragment>
+        )
+      }
+    }
+
+export default Record
